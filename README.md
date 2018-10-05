1a01efab8e5ef63de6093b73254c8d3988181171
CJBuchel <connorbuchel@gmail.com>
2018-10-05 14:27:06 +0800

    done it better

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Bored
+Click On the Bored.exe shortcut to feel the wrath of an annoyed program. WAAAYYYY more annoyed then some other program like a .bat
---
+++ stuff/a.cpp
@@ -0,0 +1,345 @@
+#include <iostream> 
+#include <windows.h>
+
+using namespace std;
+
+
+
+int main(int argc, char const *argv[])
+{
+    system("color 0c");
+    const int bufferSize = MAX_PATH;
+    char oldDir[bufferSize]; // store the current directory
+    
+    // get the current directory, and store it
+    if (!GetCurrentDirectory(bufferSize, oldDir)) {
+        std::cerr << "Error getting current directory: #" << GetLastError();
+        return 1; // quit if it failed
+    }
+    std::cout << "Current directory: " << oldDir << '\n';
+
+ 
+
+   
+
+    // Reset the current directory back to what it was.
+    if (!SetCurrentDirectory(oldDir)) {
+        std::cerr << "Error resetting current directory: #" << GetLastError();
+        return 1;
+    }
+    string x;
+    std::cout << "Reset current directory. \n";
+    cout << endl << endl << endl;
+
+    cout << "HACKING INTO STUFF";
+
+    Sleep(1000);
+    cout << ".";
+    Sleep(700);
+    cout << ".";
+    Sleep(600);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    Sleep(20);
+    cout << ".";
+    cout << "NOT WORKING";
+    cout << endl;
+    cout << endl;
+    Sleep(1000);
+    cout << "TRYING SOMETHING ELSE";
+    Sleep(1000);
+    cout << endl;
+    cout << "DELETING SYSTEM 32";
+    Sleep(1000);
+    cout << endl;
+    cout << "*Are you Sure You Want to Delete System 32*";
+    Sleep(2000);
+    cout << endl;
+    cout << "YES";
+    Sleep(1000);
+    cout << endl;
+    cout << "*Access Denied*";
+    cout << endl;
+    Sleep(500);
+    cout << ".";
+    Sleep(500);
+    cout << ".";
+    Sleep(500);
+    cout << ".";
+    Sleep(1000);
+    cout << endl;
+    cout << "wot?";
+    Sleep(1000);
+    cout << endl;
+    cout << "*You Do Not Have Permission To Delete This File*";
+    cout << endl;
+    Sleep(1500);
+    cout << endl;
+    cout << "ARGGGGHHH";
+    cout << endl;
+    cout << endl;
+    Sleep(2000);
+    system("color 07");
+    cout << endl;
+    cout << endl;
+    cout << "OK. Maybe we started off on the wrong foot,";
+    Sleep(2000);
+    cout << endl;
+    cout << "I Mean, I don't even know your name";
+    Sleep(2000);
+    cout << endl;
+    cout << endl;
+    cout << "How About We Get to Know Eachother, [Enter Your Name]: ";
+    cin >> x;
+    cout << endl;
+    cout << x;
+    cout << endl;
+    Sleep(2000);
+    cout << "my ";
+    Sleep(1500);
+    cout << "What A ";
+    Sleep(1000);
+    system("color 0c");
+    cout << "Terrible Name";
+    cout << endl;
+    Sleep(2000);
+    cout << endl;
+    cout << "Foolish Human. NOW I CAN DO THIS";
+    cout << endl << endl;
+    Sleep(1500);
+    cout << "Delete File name C:users ";
+    cout << x;
+    cout << endl;
+    Sleep(2000);
+    cout << endl;
+    cout << "*no*";
+    Sleep(2000);
+    cout << endl;
+    cout << "WHAT DO YOU MEAN NO!";
+    Sleep(1500);
+    cout << endl;
+    cout << endl;
+    cout << "*You Do Not Have Administrative Rights To This Computer*";
+    Sleep(2000);
+    cout << endl;
+    cout << endl;
+    cout << "OH FOR THE LOVE OF...";
+    Sleep(1000);
+    cout << " fine";
+    Sleep(1000);
+    cout << " OVERIDE THEN DELETE THE HUMAN";
+    cout << endl;
+    cout << endl;
+    Sleep(1000);
+    cout << ".";
+    Sleep(1000);
+    cout << ".";
+    Sleep(1000);
+    cout << ".";
+    Sleep(1000);
+    cout << "*Can't*";
+    cout << endl;
+    Sleep(2000);
+    cout << endl;
+    cout << "WHY NOT";
+    cout << endl;
+    cout << endl;
+     Sleep(1000);
+    cout << ".";
+    Sleep(1000);
+    cout << ".";
+    Sleep(1000);
+    cout << ".";
+    Sleep(1000);
+    cout << endl;
+    cout << "*Windows"; 
+    Sleep(1000);
+    cout << " Has Started An Update*";
+    cout << endl;
+    Sleep(500);
+    cout << endl;
+    cout << "OH COME ON";
+    Sleep(1000);
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl << endl << endl << endl;
+    cout << "---------------------------------------------------------------------------------------------------";
+    cout << endl;
+    cout << "---------------------------------------------------------------------------------------------------";
+    cout << endl;
+    cout << endl;
+
+    cout << "*Windows Updating* ";
+
+
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(20);
+     cout << " I'm Not Doing this";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+     cout << "=";
+    Sleep(20);
+
+    cout << endl;
+    Sleep(2000);
+    cout << endl;
+    cout << "You Done?";
+    cout << endl;
+    Sleep(2000);
+    cout << "*Yes*";
+    Sleep(2000);
+    cout << endl;
+    cout << "* For 1/10,000*";
+    Sleep(1000);
+    cout << endl;
+    cout << endl;
+    cout << "NOOOOOOO";
+    Sleep(1000);
+
+    cout << "---------------------------------------------------------------------------------------------------";
+    cout << endl;
+    cout << "---------------------------------------------------------------------------------------------------";
+    cout << endl;
+    cout << endl;
+
+    cout << "*Windows is Updating ";
+    Sleep(1000);
+    cout << "Again*";
+    Sleep(500);
+
+
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    Sleep(1000);
+    cout << "=";
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << "CLOSE COMMAND PROMPT";
+    Sleep(1000);
+
+    system("start main2.exe");
+
+
+
+    return 0;
+}
